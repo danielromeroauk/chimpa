e571664216f70b3bed1cfc9595e0d82ee2c3e8ee
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-10 22:19:14 -0500

    Envío de datos al footer mediante vars renderizando content.

--- www/applications/default/controllers/default.php
+++ www/applications/default/controllers/default.php
@@ -3,33 +3,24 @@
  * Access from index.php:
  */
 
-
 class Default_Controller extends ZP_Controller {
-	
+
 	public function __construct() {
 		$this->app("default");
-		
+
 		$this->Templates = $this->core("Templates");
 
-		$this->Templates->theme();
+		$this->Templates->theme("chimpa");
 	}
-	
-	public function index() {	
-		$vars["message"] = __("Hello World");
-		$vars["view"]	 = $this->view("show", TRUE);
-		
+
+	public function index() {
+		self::login();
+	}
+
+	public function login() {
+		$vars["view"]	 = $this->view("login", TRUE);
+		$vars["algo"] = "Si se puede renderizar información en el footer siempre y cuando se renderice content con $vars";
 		$this->render("content", $vars);
-	}
-
-	public function test($param1 = "Hola", $param2 = "Adios") {
-		print "New dispatcher it's works fine: $param1, $param2";
-	}
-
-	public function show($message) {
-		$vars["message"] = __("Hello World");
-		$vars["view"] = $this->view("show", TRUE);
-
-		$this->render("content", $vars);		
 	}
 
 }
