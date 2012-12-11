8289a7ac18cb3a542ef255f52c1f45c15540e06a
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-11 08:09:32 -0500

    Se agregó aside#lateral1

--- www/lib/themes/chimpa/content.php
+++ www/lib/themes/chimpa/content.php
@@ -1,3 +1,6 @@
 <section id="contenido">
-	<?php $this->load(isset($view) ? $view : NULL, TRUE); ?>
+	<?php #$this->load(isset($view) ? $view : NULL, TRUE); ?>
 </section>
+<aside id="lateral1">
+	<?php $this->load(isset($login) ? $login : NULL, TRUE); ?>
+</aside>
