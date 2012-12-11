4600f162d3268d3f5be03e490c123597e14fc32f
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-10 22:17:33 -0500

    Formulario de login

---
+++ www/applications/default/views/login.php
@@ -0,0 +1,8 @@
+<section id="logueo">
+	<form>
+	  <label for="user">Usuario:</label>
+	  <input type="text" name="user" id="user" />
+	  <label for="pass">Password:</label>
+	  <input type="password" name="pass" id="pass" />
+	</form>
+</section>
