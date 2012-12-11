8d9d8793a8117a4202fb9cc1edfcdcba7e50828e
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-11 08:05:33 -0500

    Se quitó el test de $algo

--- www/lib/themes/chimpa/footer.php
+++ www/lib/themes/chimpa/footer.php
@@ -2,8 +2,6 @@
 
     <footer>
       <p>&copy; <?php print __("All rights reserved"); ?> - ZanPHP Framework v.2.6.8 - 2012 - <?php print __("Powered by"); ?> <a href="http://www.adsiar.blogspot.com" title="ADSIAR">ADSIAR</a></p>
-
-      <?php print $algo; ?>
     </footer>
   </body>
 </html>
