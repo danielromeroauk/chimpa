b2257f9f1c6d67e3cd36ab0c11a6a6af58ed2502
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-11 15:37:17 -0500

    KendoMenu

--- www/lib/themes/chimpa/header.php
+++ www/lib/themes/chimpa/header.php
@@ -4,8 +4,12 @@
 		<meta charset="utf-8" />
 		<title><?php print $this->getTitle(); ?></title>
 
-		<link type="text/css" href="<?php print path("vendors/css/frameworks/bootstrap/bootstrap.min.css", "zan"); ?>" rel="stylesheet" />
-		<link type="text/css" href="<?php print $this->themePath; ?>/css/style.css" rel="stylesheet" />
+		<link rel="stylesheet" href="<?php print path("vendors/css/frameworks/bootstrap/bootstrap.min.css", "zan"); ?>" />
+
+		<link href="<?php print $this->themePath; ?>/css/kendo.common.min.css" rel="stylesheet" />
+    <link href="<?php print $this->themePath; ?>/css/kendo.default.min.css" rel="stylesheet" />
+
+		<link href="<?php print $this->themePath; ?>/css/style.css" rel="stylesheet" />
 
 		<?php print $this->getCSS(); ?>
 
@@ -14,6 +18,14 @@
 			  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
 			<![endif]-->
 		<!-- Le styles -->
+
+		<!--<script src="<?php #print $this->themePath; ?>/js/jquery.min.js"></script>-->
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="<?php print $this->themePath; ?>/js/kendo.web.min.js"></script>
+    <script src="<?php print $this->themePath; ?>/js/chimpa.js"></script>
+
+    <?php print $this->getJs(); ?>
+
 	</head>
 
 	<body>
@@ -21,4 +33,34 @@
       <header>
         <h1>Construimportados.com <small>Control de inventarios</small></h1>
       </header>
-      <nav id="menu-principal"></nav>
+      <nav id="menu-principal">
+      	<ul>
+      		<li>Lugar<ul><li>Agregar</li><li>Editar</li><li>Listado</li></ul></li>
+    			<li>Usuarios
+            <ul>
+        			<li><a href="#">Agregar</a></li>
+        			<li><a href="#">Editar</a></li>
+        			<li><a href="#">Listado</a></li>
+            </ul>
+          </li>
+    			<li>Remisiones
+            <ul>
+        			<li>Compra</li>
+        			<li>Venta</li>
+        			<li>Rotación</li>
+        			<li>Entrega inmediata</li>
+            </ul>
+          </li>
+    			<li>Artículos
+            <ul>
+        			<li>Agregar</li>
+        			<li>Editar</li>
+        			<li>Listado</li>
+        			<li>Extracto</li>
+        			<li>Apartar</li>
+            </ul>
+          </li>
+    			<li>Remisiones pendientes</li>
+    			<li>Cerrar sesión</li>
+      	</ul>
+      </nav>
