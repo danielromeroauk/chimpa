5621a06a0208894917c7a08602dc15134296b75d
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-11 11:21:47 -0500

    Todo vuelve a la normalidad.

--- www/applications/default/controllers/default.php
+++ www/applications/default/controllers/default.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-/**
- * Access from index.php:
- */
-
-
-class Default_Controller extends ZP_Controller {
-	
-	public function __construct() {
-		$this->app("default");
-		
-		$this->Templates = $this->core("Templates");
-
-		$this->Templates->theme();
-	}
-	
-	public function index() {	
-		$vars["message"] = __("Hello World");
-		$vars["view"]	 = $this->view("show", TRUE);
-		
-		$this->render("content", $vars);
-	}
-
-	public function test($param1 = "Hola", $param2 = "Adios") {
-		print "New dispatcher it's works fine: $param1, $param2";
-	}
-
-	public function show($message) {
-		$vars["message"] = __("Hello World");
-		$vars["view"] = $this->view("show", TRUE);
-
-		$this->render("content", $vars);		
-	}
-
-}
-=======
 <?php
 /**
  * Access from index.php:
@@ -60,4 +23,3 @@ class Default_Controller extends ZP_Controller {
 	}
 
 }
->>>>>>> 34ecd9d6a1b62d26de8547b3449888ef1c3185f7
--- www/lib/themes/chimpa/content.php
+++ www/lib/themes/chimpa/content.php
@@ -1,6 +1,6 @@
 <section id="contenido">
-	<?php #$this->load(isset($view) ? $view : NULL, TRUE); ?>
+	<?php #$this->load(isset($view) ? $view : "NULL", TRUE); ?>
 </section>
-<aside id="lateral1">
-	<?php $this->load(isset($login) ? $login : NULL, TRUE); ?>
-</aside>
+<section id="lateral1">
+  <?php $this->load(isset($login) ? $login : NULL, TRUE); ?>
+</section>
