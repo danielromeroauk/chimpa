dbdc123178523eb81e6280579840da7c92c629ac
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-12 16:06:56 -0500

    Modelo de la tabla usuario.
    Valida un usuario, creando la variable de sesión user si existe un
    registro relacionado.

---
+++ www/applications/default/models/usuario.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Access from index.php:
+ */
+if(!defined("_access")) {
+	die("Error: You don't have permission to access here...");
+}
+
+class Usuario_Model extends ZP_Model {
+
+	public function __construct() {
+		$this->Db = $this->db();
+		$this->table = "usuario";
+		$this->fields = "nickname, password";
+	}
+
+	public function valido($nickname, $password) {
+		#TODO: Cambiar la longitud del campo password en construi_inventario.usuario de 20 a 40
+		$data = $this->Db->findBySQL("nickname='$nickname' AND password=SHA1('$password')", $this->table, $this->fields);
+		if ($data) {
+			SESSION("user", $nickname);
+			return TRUE;
+		} else {
+			return getAlert("El usuario o password incorrecto.");
+		}
+	}
+
+}
