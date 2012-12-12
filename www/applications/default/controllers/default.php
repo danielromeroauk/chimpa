75f6f4b98704e334a7afd2ac405b3bb989ce7ad0
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-12 16:07:56 -0500

    Oculta el formulario de login al inciar sesión.

--- www/applications/default/controllers/default.php
+++ www/applications/default/controllers/default.php
@@ -14,12 +14,36 @@ class Default_Controller extends ZP_Controller {
 	}
 
 	public function index() {
-		self::login();
+    self::login();
+  }
+
+  public function login() {
+		$this->title("Login");
+    $this->Usuario_Model = $this->model("Usuario_Model");
+
+    if (POST('entrar')) {
+      $vars["alert"] = $this->Usuario_Model->valido(POST("user"), POST("pass"));
+      if ($vars["alert"] === TRUE) {
+        self::inicio();
+      }
+    }
+
+    if (!POST('entrar') || $vars["alert"] !== TRUE) {
+      $vars["login"] = $this->view("login", TRUE);
+      $this->render("content", $vars);
+    }
+
 	}
 
-	public function login() {
-		$vars["login"]	 = $this->view("login", TRUE);
-		$this->render("content", $vars);
-	}
+  public function inicio() {
+    $this->title("Inicio");
+    $vars["message"] = "Estamos dentro";
+    $vars["view"] = $this->view("show", TRUE);
+    $this->render("content", $vars);
+  }
+
+  public function logout() {
+      unsetSessions();
+  }
 
 }
