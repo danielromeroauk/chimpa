5215d5d94a9bdedc775a54de16955a733bec0104
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-12 16:01:45 -0500

    Mensaje $alert y botón entrar.

--- www/applications/default/views/login.php
+++ www/applications/default/views/login.php
@@ -1,9 +1,13 @@
 <section id="logueo">
+	<p>
+    <?php echo isset($alert) ? $alert : NULL; ?>
+  </p>
   <img src="<?php print $this->themePath; ?>/img/inventario.jpg" />
-	<form>
+	<form action="<?php echo get("webURL"); ?>/default/default/login" method="POST">
 	  <label for="user">Usuario:</label>
 	  <input type="text" name="user" id="user" />
 	  <label for="pass">Password:</label>
 	  <input type="password" name="pass" id="pass" />
+	  <p><input type="submit" name="entrar" id="entrar" value="Entrar" /></p>
 	</form>
 </section>
