3a373d0191e5ed34d82054232e47e19244b91876
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-16 18:17:08 -0500

    Modelo establecimiento, save()

--- www/applications/default/models/establecimiento.php
+++ www/applications/default/models/establecimiento.php
@@ -12,10 +12,35 @@ class Establecimiento_Model extends ZP_Model {
 		$this->Db = $this->db();
 		$this->table = "establecimiento";
 		$this->fields = "id, nombre, direccion, telefono";
+		$this->Data = $this->core("Data");
 	}
 
 	public function getListado() {
 		return $this->Db->findAll($this->table, $this->fields);
+	}
+
+	public function save() {
+		$validations = array(
+			"id" => "required",
+			"nombre" => "required",
+			"direccion" => "required",
+			"telefono" => "required",
+		);
+
+    $this->Data->ignore("save");
+
+    $data = $this->Data->proccess(NULL, $validations);
+
+		if(isset($data["error"])) {
+      return $data["error"];
+		} else {
+			if($this->Db->insert($this->table, $data) === FALSE) {
+       return getAlert("El ID del establecimiento ya existe.");
+      } else {
+       return getAlert("El establecimiento fue registrado exitosamente.", "success");
+      }
+		}
+
 	}
 
 }
