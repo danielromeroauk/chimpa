ea1e7013aa9f8814cd39cc09967692b1a29eb253
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-16 19:56:04 -0500

    Controlador establecimiento, permisos establecidos.

--- www/applications/default/controllers/establecimiento.php
+++ www/applications/default/controllers/establecimiento.php
@@ -5,38 +5,43 @@
 
 class Establecimiento_Controller extends ZP_Controller {
 
-	public function __construct() {
-		$this->app("default");
+  public function __construct() {
+    $this->app("default");
 
-		$this->Templates = $this->core("Templates");
+    $this->Templates = $this->core("Templates");
 
-		$this->Templates->theme();
-	}
+    $this->Templates->theme();
+  }
 
-	public function index() {
+  public function index() {
 
-	}
+  }
 
-	public function add() {
-	    $this->title("Agregar establecimiento");
-	    $this->Establecimiento_Model = $this->model("Establecimiento_Model");
+  public function add() {
+    isConnected(get("webURL"));
+    if(!in_array(SESSION("rol"), array(0)) { redirect(get("webURL") .'/default/default/inicio'); }
 
-	    if (POST('save')) {
-	      $vars["alert"] = $this->Establecimiento_Model->save();
-	    }
+    $this->title("Agregar establecimiento");
+    $this->Establecimiento_Model = $this->model("Establecimiento_Model");
 
-  		$vars["view"] = $this->view("establecimiento", TRUE);
-  		$this->render("content", $vars);
-	}
+    if (POST('save')) {
+      $vars["alert"] = $this->Establecimiento_Model->save();
+    }
 
-	public function edit() {
+    $vars["view"] = $this->view("establecimiento", TRUE);
+    $this->render("content", $vars);
+  }
 
-	}
+  public function edit() {
 
-	public function listado() {
-	    $this->Establecimiento_Model = $this->model("Establecimiento_Model");
-		$data = $this->Establecimiento_Model->getListado();
-		____($data);
-	}
+  }
+
+  public function listado() {
+    if(!in_array(SESSION("rol"), array(0)) { redirect(get("webURL") .'/default/default/inicio'); }
+
+    $this->Establecimiento_Model = $this->model("Establecimiento_Model");
+    $data = $this->Establecimiento_Model->getListado();
+    ____($data);
+  }
 
 }
