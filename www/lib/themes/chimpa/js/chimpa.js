51af7aa5bd3857e10c8005fc80ed3a5b3b04ad20
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-17 18:21:45 -0500

    Botones de editar con JQueryUI.

--- www/lib/themes/chimpa/js/chimpa.js
+++ www/lib/themes/chimpa/js/chimpa.js
@@ -1,6 +1,13 @@
 $(document).on("ready", empezar);
 
 function empezar() {
-	$("#menu-principal ul").kendoMenu();
-	$("input[type='submit']").button();
-}
+  $("#menu-principal ul").kendoMenu();
+  $("input[type='submit']").button();
+
+  $(".boton-edit").button({
+      icons: {
+        primary: "ui-icon-pencil"
+      }
+  });
+
+} // empezar
