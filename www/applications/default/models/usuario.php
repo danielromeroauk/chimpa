114de1f0aa45c885d7057a48a7cee533115e9195
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-16 19:57:02 -0500

    Variable de sesión: rol

--- www/applications/default/models/usuario.php
+++ www/applications/default/models/usuario.php
@@ -11,14 +11,15 @@ class Usuario_Model extends ZP_Model {
 	public function __construct() {
 		$this->Db = $this->db();
 		$this->table = "usuario";
-		$this->fields = "nickname, password";
+		$this->fields = "nickname, password, rol";
 	}
 
 	public function valido($nickname, $password) {
 		#TODO: Cambiar la longitud del campo password en construi_inventario.usuario de 20 a 40
 		$data = $this->Db->findBySQL("nickname='$nickname' AND password=SHA1('$password')", $this->table, $this->fields);
 		if ($data) {
-			SESSION("user", $nickname);
+			SESSION("user", $data[0]['nickname']);
+			SESSION("rol", $data[0]['rol']);
 			return TRUE;
 		} else {
 			return getAlert("El usuario o password incorrecto.");
