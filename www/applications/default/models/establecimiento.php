ffe7eb8216372e768e2cc442d04651601a7f9557
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-16 19:54:41 -0500

    Modelo establecimiento, getById()

--- www/applications/default/models/establecimiento.php
+++ www/applications/default/models/establecimiento.php
@@ -3,44 +3,47 @@
  * Access from index.php:
  */
 if(!defined("_access")) {
-	die("Error: You don't have permission to access here...");
+  die("Error: You don't have permission to access here...");
 }
 
 class Establecimiento_Model extends ZP_Model {
 
-	public function __construct() {
-		$this->Db = $this->db();
-		$this->table = "establecimiento";
-		$this->fields = "id, nombre, direccion, telefono";
-		$this->Data = $this->core("Data");
-	}
+  public function __construct() {
+    $this->Db = $this->db();
+    $this->table = "establecimiento";
+    $this->fields = "id, nombre, direccion, telefono";
+    $this->Data = $this->core("Data");
+  }
 
-	public function getListado() {
-		return $this->Db->findAll($this->table, $this->fields);
-	}
+  public function getListado() {
+    return $this->Db->findAll($this->table, $this->fields);
+  }
 
-	public function save() {
-		$validations = array(
-			"id" => "required",
-			"nombre" => "required",
-			"direccion" => "required",
-			"telefono" => "required",
-		);
+  public function save() {
+    $validations = array(
+      "id" => "required",
+      "nombre" => "required",
+      "direccion" => "required",
+      "telefono" => "required",
+    );
 
-    $this->Data->ignore("save");
+      $this->Data->ignore("save");
 
-    $data = $this->Data->proccess(NULL, $validations);
+      $data = $this->Data->proccess(NULL, $validations);
 
-		if(isset($data["error"])) {
-      return $data["error"];
-		} else {
-			if($this->Db->insert($this->table, $data) === FALSE) {
-       return getAlert("El ID del establecimiento ya existe.");
-      } else {
+    if(isset($data["error"])) {
+          return $data["error"];
+    } elseif($this->Db->insert($this->table, $data) === FALSE) {
+        return getAlert("El ID del establecimiento ya existe.");
+    } else {
        return getAlert("El establecimiento fue registrado exitosamente.", "success");
-      }
-		}
+    }
 
-	}
+  } #save
 
-}
+  public function getById($id) {
+    $data = $this->Db->find($id, $this->table, $this->fields);
+    return $data[0];
+  }
+
+} #Establecimiento_Model
