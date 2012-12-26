4eaf1755c74629121cd43c77e2354304d10ea9b0
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2012-12-26 16:42:25 -0500

    button de paginación con JQueryUI

--- www/lib/themes/chimpa/js/chimpa.js
+++ www/lib/themes/chimpa/js/chimpa.js
@@ -4,6 +4,8 @@ function empezar() {
   $("#menu-principal ul").kendoMenu();
   $("input[type='submit']").button();
 
+  $("button").button();
+
   $(".boton-edit").button({
       icons: {
         primary: "ui-icon-pencil"
