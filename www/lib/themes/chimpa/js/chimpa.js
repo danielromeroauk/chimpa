c4f76895e64b21aa5416d9b98690c1bde0f09664
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2013-03-23 23:41:44 -0500

    Uso de bootstrap para botones con ayuda de JQuery.

--- www/lib/themes/chimpa/js/chimpa.js
+++ www/lib/themes/chimpa/js/chimpa.js
@@ -1,9 +1,8 @@
 $(document).on("ready", empezar);
 
 function empezar() {
-  $("input[type='submit']").button();
 
-  $("button").button();
+  $("button, input[type='submit']").addClass('btn');
 
   $(".boton-edit").button({
       icons: {
