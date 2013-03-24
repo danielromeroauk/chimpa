59758af34a110fad2db5244e34ec292d1417ffcd
Daniel Guillermo Romero Gelvez <danielromeroauk@gmail.com>
2013-03-23 23:34:08 -0500

    Se usa booststrap en el botón Entrar.
    Autofocus en campo user.

--- www/applications/default/views/login.php
+++ www/applications/default/views/login.php
@@ -5,9 +5,9 @@
   <img src="<?php print $this->themePath; ?>/img/inventario.jpg" />
 	<form action="<?php echo get("webURL"); ?>/default/default/login" method="POST">
 	  <label for="user">Usuario:</label>
-	  <input type="text" name="user" id="user" required />
+	  <input type="text" name="user" id="user" autofocus required />
 	  <label for="pass">Password:</label>
 	  <input type="password" name="pass" id="pass" required />
-	  <p><input type="submit" name="entrar" id="entrar" value="Entrar" /></p>
+	  <p><input type="submit" name="entrar" id="entrar" value="Entrar" class="btn btn-inverse" /></p>
 	</form>
 </section>
